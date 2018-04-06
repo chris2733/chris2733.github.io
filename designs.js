e95a981c4daae0bbdd656306e6c753ce115e5280
Chris Law <>
2018-04-06 15:13:08 +0100

    v6
    Replacing the contact form with a mailto, moving the social inks up to here, adding a link to my projects with the zenpencils in bootstrap, and adding the rest of my portfolio peices

--- designs.js
+++ designs.js
@@ -2,7 +2,29 @@
 // Notes
 //use $(window) so it shows only the viewport, essentially, use this later
 
-//loading page
+
+
+// fading in items .hideme on scroll
+$(function(){  // $(document).ready shorthand
+  $('').fadeIn('slow');
+});
+
+$(document).ready(function() {
+    /* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+        /* Check the location of each desired element */
+        $('.hideuntilscroll').each( function(i){
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).delay(100).animate({'opacity':'1'}, 600);
+            }
+        });
+    });
+});
 
 
 
@@ -127,152 +149,6 @@ $('#starticon3').click(scroll3);
 
 
 
-
-// make label text i form disappear when clicked on and if text is typed
-$("input[type=name]").focus(onFocus);
-$("input[type=email]").focus(onFocus);
-$("textarea").focus(onFocus);
-
-$("input[type=name]").focusout(onFocusOut);
-$("input[type=email]").focusout(onFocusOut);
-$("textarea").focusout(onFocusOut);
-
-function onFocus(){
-		$(this).parent().find("label").addClass("activeinput");
-};
-function onFocusOut(){
-	if($(this).val() === "")
-		$(this).parent().find("label").removeClass("activeinput");
-};
-
-//  edit the sumbit button later, make it scroll up & turn
-//  green for ok, scroll down and turn red for not.
-// see https://codepen.io/paulstamp/pen/qagBpL
-
-
-
-// clearing form, for use in submitting form
-
-var phpURL = "throughmessage.php";
-
-
-
-function validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-}
-
-function validateForm(){
-
-		var email = $("input[type=email]").val();
-		var validEmail = validateEmail(email);
-		var validName = $("input[type=name]").val() ? true : false;
-		var validMsg = $("textarea").val() ? true : false;
-
-		var nameError = $("input[type=name]").parent().find(".error-mark");
-		var emailError = $("input[type=email]").parent().find(".error-mark");
-		var msgError = $("textarea").parent().find(".error-mark");
-
-		TweenLite.to(nameError, 0.5, {opacity: validName ? 0 : 1});
-		TweenLite.to(emailError, 0.5, {opacity: validEmail ? 0 : 1});
-		TweenLite.to(msgError, 0.5, {opacity: validMsg ? 0 : 1});
-
-	return (validEmail && validName && validMsg);
-}
-
-function flashButton(color){
-	var originalColor = "rgba(255, 255, 255, 0.3)";
-  /*
-	setTimeout(function(){
-		$("#actualbutton").css("background-color", color);
-	},200);
-
-	setTimeout(function(){
-		$("#actualbutton").css("background-color", originalColor);
-	},1600); */
-
-  if (color==="#4cd137") {
-    setTimeout(function(){
-  		$("#actualbutton").addClass("buttongreen");
-    },200);
-  setTimeout(function(){
-		$("#actualbutton").removeClass("buttongreen");
-	},1600);
-  }
-  else {
-    setTimeout(function(){
-  		$("#actualbutton").addClass("buttonred");
-    },200);
-  setTimeout(function(){
-		$("#actualbutton").removeClass("buttonred");
-	},1600);
-}
-
-}
-
-function sentAnimation(message, color) {
-
-		flashButton(color);
-		var sentElement = $(".result-message");
-		$(sentElement).text(message);
-		var sentTween = new TimelineLite();
-		sentTween.fromTo(sentElement, 0.6, {y:0},{ y:70});
-		sentTween.to(sentElement, 0.6, {y:140, delay:1});
-
-		var envelopeElement = $(".submit-icon");
-		var envelopeTween = new TimelineLite();
-		envelopeTween.fromTo(envelopeElement, 0.6, {y:0}, {y:70});
-		envelopeTween.fromTo(envelopeElement, 0.6, {y:-70}, {y:0, delay:1});
-
-}
-
-
-
-function clearForm(){
-	$("input[type=email]").val("");
-	$("input[type=name]").val("");
-	$("textarea").val("");
-
-  $("input[type=name]").focusout(onFocusOut);
-  $("input[type=email]").focusout(onFocusOut);
-  $("textarea").focusout(onFocusOut);
-}
-
-
-$(function() {
-  $('#thisistheform').submit(function(event) {
-    //event.preventDefault();
-
-		if(validateForm()){
-			sentAnimation("Sent", "#4cd137");
-
-			//post form
-			var formEl = $(this);
-			console.log(formEl.serialize());
-			var submitButton = $('input[type=submit]', formEl);
-
-			$.ajax({
-				type: 'POST',
-				url: phpURL,
-				accept: {
-					javascript: 'application/javascript'
-				},
-				data: formEl.serialize(),
-				beforeSend: function() {
-					submitButton.prop('disabled', 'disabled');
-				}
-			}).done(function(data) {
-				submitButton.prop('disabled', false);
-			});
-
-			clearForm();
-
-		}
-		else{
-			sentAnimation("Error", "#c23616");
-		}
-  });
-});
 
 
 
