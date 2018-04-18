abcb9b4ff530d0eb172de58ef9f63cb99dacad3e
Chris Law <>
2018-04-19 00:21:45 +0100

    Small edits further
    Adding project logos

--- designs_v5.1.js
+++ designs_v5.1.js
@@ -124,28 +124,15 @@ function showPage() {
 
 
 // Menu icon and items animations
-
+/*
 const menuIcon = document.querySelector('#menuicon');
 
 function menuAnimation() {
     $('#menuicon').toggleClass("clickMenuSpan");
     $('#menuicon').toggleClass("rotateMenu");
 };
-
+*/
 //menuIcon.addEventListener('click', menuAnimation);
-
-// since scrollinotview doesnt work too well on certain browxers, use more js instead
-function getOffset( el ) { // to find position of element on page
-    var _x = 0;
-    var _y = 0;
-    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
-        _x += el.offsetLeft - el.scrollLeft;
-        _y += el.offsetTop - el.scrollTop;
-        el = el.offsetParent;
-    }
-    return { top: _y, left: _x };
-}
-var x = getOffset( document.getElementById('yourElId') ).left;
 
 
 
