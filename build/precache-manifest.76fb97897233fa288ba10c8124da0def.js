76a1fa8f7ada058fe81585b2d9ad886720325f00
Chris Law <CLaw@spindogs.com>
2019-02-18 10:29:25 +0000

    New build, again
    Trying again, this time taking prebuild stuff out to a separate folder

---
+++ build/precache-manifest.76fb97897233fa288ba10c8124da0def.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "991c0eb6a8b92cff5d4d",
+    "url": "/static/js/main.991c0eb6.chunk.js"
+  },
+  {
+    "revision": "1015926caf23046f2ba3",
+    "url": "/static/js/1.1015926c.chunk.js"
+  },
+  {
+    "revision": "991c0eb6a8b92cff5d4d",
+    "url": "/static/css/main.afb2f3ae.chunk.css"
+  },
+  {
+    "revision": "5af91acb52ec6183c448bab4e5de97a3",
+    "url": "/index.html"
+  }
+];
