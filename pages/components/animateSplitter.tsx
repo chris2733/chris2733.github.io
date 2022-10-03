6b906e34283489ce156c591646d7a1233b9bef2c
Chris Law <chris_law9208@hotmail.co.uk>
2022-10-03 01:40:14 +0100

    fix non iterable array err

--- pages/components/animateSplitter.tsx
+++ pages/components/animateSplitter.tsx
@@ -6,37 +6,40 @@ export default function Splitter({
 	letterDelay,
 	letterClass,
 }: {
-	text: string;
+	text?: string;
 	initialDelay?: number;
 	letterDelay?: number;
 	letterClass?: string;
 }) {
-	let splittext = Array.from(text);
+	// check over this, needed to check if text was undefined so it didnt throw a non iterable error
+	const splittext: string[] = text !== undefined ? Array.from(text) : [""];
+
 	return (
 		<>
-			{splittext.map((el, i) => (
-				<motion.span
-					key={`splitter${i}`}
-					initial="hidden"
-					animate="visible"
-					variants={{
-						hidden: {
-							y: "100%",
-						},
-						visible: {
-							y: "0",
-							transition: {
-								delay:
-									i * (letterDelay ? letterDelay : 0.2) +
-									(initialDelay ? initialDelay : 0),
+			{splittext !== undefined &&
+				splittext.map((el, i) => (
+					<motion.span
+						key={`splitter${i}`}
+						initial="hidden"
+						animate="visible"
+						variants={{
+							hidden: {
+								y: "100%",
 							},
-						},
-					}}
-					className={letterClass ? letterClass : "duration-200"}
-				>
-					<span dangerouslySetInnerHTML={{ __html: el }}></span>
-				</motion.span>
-			))}
+							visible: {
+								y: "0",
+								transition: {
+									delay:
+										i * (letterDelay ? letterDelay : 0.2) +
+										(initialDelay ? initialDelay : 0),
+								},
+							},
+						}}
+						className={letterClass ? letterClass : "duration-200"}
+					>
+						<span dangerouslySetInnerHTML={{ __html: el }}></span>
+					</motion.span>
+				))}
 		</>
 	);
 }
