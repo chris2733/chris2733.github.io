cf00680a61507540e49ebfefc453f6ed1770c3c2
Chris Law <chris_law9208@hotmail.co.uk>
2022-10-03 01:46:28 +0100

    add hreflang tag

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+	reactStrictMode: true,
+	swcMinify: true,
+	i18n: {
+		locales: ["en-GB", "en-US"],
+		defaultLocale: "en-GB",
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
